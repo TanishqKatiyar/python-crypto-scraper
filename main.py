2cd813bc56a1286763f41680e779f5908c1fd610
Tanishq Katiyar <2k22.cse.2213339@gmail.com>
2025-11-06 18:10:06 +0530

    Add data visualization functions

--- main.py
+++ main.py
@@ -28,3 +28,8 @@ if __name__ == '__main__':
 if __name__ == '__main__':
     print('Running scraper...')
 
+try:
+    results = parse_html(content)
+except Exception as e:
+    logging.error(f'Error parsing: {e}')
+
